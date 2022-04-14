dd067fddb51554e547cbd0e0c849a01c57ae7e61
Al Nahian <a.alnahian2003@gmail.com>
2022-04-15 02:11:59 +0600

    Basic Regular Expression Completed

--- basic/regex.php
+++ basic/regex.php
@@ -5,3 +5,22 @@
  * Author: Al Nahian (alnahian2003)
  * Topic: RegEx - Regular Expressions
  */
+
+// Basic Getting Started With RegEx in PHP
+$str = "Hello, My Name is alnahian2003";
+$exp = "/nahian/";
+
+// Match the string 'nahian' in $str case-insensitively using regEx.
+function matchString($expression, $string)
+{
+    if (preg_match($expression, $string)) {
+        // Return 'Found' if the matched string found
+        return "Found :D";
+    } else {
+        // Return 'Sorry, Not Found' if can't match the string
+        return "Sorry, Not Found :(";
+    }
+}
+
+// Let's test this simple case
+echo matchString($exp, $str);
