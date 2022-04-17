1a495d7f071492f91aa283df8ee68be26050f4d0
Al Nahian <a.alnahian2003@gmail.com>
2022-04-18 03:38:56 +0600

    Topic Files in PHP Created

---
+++ advanced/php-file/file-handling.php
@@ -0,0 +1,7 @@
+<?php
+/* 
+ * Date: 18 April, 2022
+ * Time: 3:25 AM, 18 April, 2022
+ * Author: Al Nahian (alnahian2003)
+ * Topic: Handling files to perform any CRUD operation on them
+ */
---
+++ advanced/php-file/file-upload.php
@@ -0,0 +1,7 @@
+<?php
+/* 
+ * Date: 18 April, 2022
+ * Time: 
+ * Author: Al Nahian (alnahian2003)
+ * Topic: Working Uploading Files in PHP
+ */
