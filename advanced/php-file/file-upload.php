6e880f7e77376a8905b2b029def0ff7f9383567b
Al Nahian <a.alnahian2003@gmail.com>
2022-04-20 01:01:30 +0600

    Reading a Directory Data and Implemented on Project

--- advanced/php-file/file-upload.php
+++ advanced/php-file/file-upload.php
@@ -25,49 +25,58 @@ $supportedFileType = array("png", "jpg", "jpeg");
 if (isset($_POST["upload"])) {
     $uploadedFile = $_FILES["uploadImage"];
 
-    $targetFile = $targetDirectory . basename($uploadedFile["name"]);
-    $temp_name = $uploadedFile["tmp_name"];
+    // Check for if the uploaded file is empty or not
+    if (($uploadedFile["size"] != 0) && $uploadedFile["error"] == 0) {
+        $targetFile = $targetDirectory . basename($uploadedFile["name"]);
+        $temp_name = $uploadedFile["tmp_name"];
 
-    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($temp_name);
-    if (!$check == false) {
-        $uploaded = 1;
-    } else {
-        echo "Please upload an image. </br>";
-        $uploaded = 0;
-    } // Aborts the upload
-
-    // Check for file already exists
-    if (file_exists($targetFile)) {
-        echo "File already exists! </br>";
-        $uploaded = 0;
-    }
-
-    // Limit File Size (Max 500kb)
-    if (filesize($temp_name) > (1000 * 500)) {
-        echo "Sorry, the image size is too large. Max 500 kilobytes! </br>";
-        $uploaded = 0;
-    }
-
-    // Limit File Type (Only PNG,JPG,JPEG)
-    if (!in_array($imageFileType, $supportedFileType)) {
-        echo "Only PNG/JPG/JPEG files are allowed! </br>";
-        $uploaded = 0;
-    }
-
-    // Check if $uploaded = 0 set for any error. It will stop the file upload
-    if ($uploaded == 0) {
-        "Sorry, your file was not uploaded successfully!";
-    } else {
-        if (move_uploaded_file($temp_name, $targetFile)) {
-            echo htmlspecialchars(basename($uploadedFile["name"])) . " has been uploaded successfully!";
+        // Check if image file is a actual image or fake image
+        $check = getimagesize($temp_name);
+        if (!$check == false) {
+            $uploaded = 1;
         } else {
-            echo "Sorry, there was an error while uploading";
+            echo "Please upload an image. </br>";
+            $uploaded = 0;
+        } // Aborts the upload
+
+        // Check for file already exists
+        if (file_exists($targetFile)) {
+            echo "File already exists! </br>";
+            $uploaded = 0;
         }
+
+        // Limit File Size (Max 500kb)
+        if (filesize($temp_name) > (1000 * 500)) {
+            echo "Sorry, the image size is too large. Max 500 kilobytes! </br>";
+            $uploaded = 0;
+        }
+
+        // Limit File Type (Only PNG,JPG,JPEG)
+        if (!in_array($imageFileType, $supportedFileType)) {
+            echo "Only PNG/JPG/JPEG files are allowed! </br>";
+            $uploaded = 0;
+        }
+
+        // Check if $uploaded = 0 set for any error. It will stop the file upload
+        if ($uploaded == 0) {
+            "Sorry, your file was not uploaded successfully!";
+        } else {
+            if (move_uploaded_file($temp_name, $targetFile)) {
+                echo htmlspecialchars(basename($uploadedFile["name"])) . " has been uploaded successfully!";
+            } else {
+                echo "Sorry, there was an error while uploading";
+            }
+        }
+    } else {
+        echo "You must select an image to upload.";
     }
 }
+
+// Work with directory
+$openDirectory = @opendir($targetDirectory) or die("Unable to open folder"); // Open "uploads/" file where all images uploaded
+$dirContent = scandir($targetDirectory);
 
 ?>
 
@@ -80,6 +89,30 @@ if (isset($_POST["upload"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Uploader</title>
+
+    <style>
+        html,
+        body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+
+        .avatars {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(180px, 4fr));
+            grid-auto-rows: auto;
+            grid-column-gap: 10px;
+            grid-row-gap: 10px;
+            justify-items: center;
+            justify-items: center;
+            align-items: center;
+            text-align: center;
+        }
+
+        img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
 </head>
 
 <body>
@@ -94,6 +127,28 @@ if (isset($_POST["upload"])) {
         <br>
         <input type="submit" name="upload" value="Upload">
     </form>
+
+    <!-- Diplay all the uploaded images from "uploads/" directory -->
+    <div class="avatars">
+
+        <?php
+        foreach ($dirContent as $key => $imgName) {
+            $imgPath = $targetDirectory . $imgName;
+
+            // Extract File Name for Using them as a fictional User name
+            $userName = explode(".", $imgName);
+            $userName = $userName[0];
+            $userName = ucfirst($userName);
+
+            if ($imgName != '.' && $imgName != '..') {
+                echo "<div class='avatar'>"; // Begin the .avatar Div
+                echo "<img src='{$imgPath}' alt='{$userName}'/>";
+                echo "<p>{$userName}</p>";
+                echo "</div>"; // End the .avatar div
+            }
+        }
+        ?>
+    </div>
 </body>
 
 </html>
