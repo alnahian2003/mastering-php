77a8856347c6a12f74ce8cb3162bd27139316a3d
Al Nahian <a.alnahian2003@gmail.com>
2022-04-25 02:20:20 +0600

    "callback.php" is ready to go!

--- advanced/callback.php
+++ advanced/callback.php
@@ -6,4 +6,20 @@
  * Topic: Working With Callback Function in PHP
  */
 
- 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Callback Functions in PHP</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
