a9819da79f36b17d054eea1b1a1d5fbaa38a1884
Al Nahian <a.alnahian2003@gmail.com>
2022-04-26 23:27:54 +0600

    Working with constructor and destructor in php completed

--- advanced/oop/constructor.php
+++ advanced/oop/constructor.php
@@ -5,3 +5,29 @@
  * Author: Al Nahian (alnahian2003)
  * Topic: Working With Constructor in PHP OOP
  */
+
+class Person
+{
+    public $name;
+    public $age;
+    public $gender;
+
+    function __construct($name, int $age, $gender)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+}
+
+$person1 = new Person("Al Nahian", 19, "Male");
+
+$name = $person1->getName();
+$age = $person1->age;
+$gender = $person1->gender;
+echo "$name is a $age years old $gender";
--- advanced/oop/destructor.php
+++ advanced/oop/destructor.php
@@ -5,3 +5,59 @@
  * Author: Al Nahian (alnahian2003)
  * Topic: Working With Destructor in PHP OOP
  */
+
+class Computer
+{
+    public $name;
+    public $price;
+    function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    function __destruct()
+    {
+        $this->name;
+        $this->price;
+    }
+}
+
+
+$pc = new Computer("Dell", 300);
+
+echo "PC name is {$pc->name} and the price is: $$pc->price";
+
+echo nl2br("\n");
+
+class File
+{
+    public $fileName;
+    public $handler;
+
+    function __construct($fileName, $mode = "r")
+    {
+        $this->fileName = $fileName;
+        // open the file
+        $this->handler = fopen($fileName, $mode) or die("Couldn't find the file!");
+    }
+
+    function __destruct()
+    {
+        // close the file
+        if ($this->handler) {
+            fclose($this->handler);
+        }
+    }
+
+    function readFile()
+    {
+        // read file contents
+        return fread($this->handler, filesize($this->fileName));
+    }
+}
+
+
+$readme = new File("../../readme.md");
+
+print_r($readme->readfile());
