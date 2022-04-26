2cfa101680a4517e82b1a7c6b0f61afb52922378
Al Nahian <a.alnahian2003@gmail.com>
2022-04-26 13:16:26 +0600

    Working with basic exceptions completed

--- advanced/exceptions.php
+++ advanced/exceptions.php
@@ -1,7 +1,37 @@
 <?php
 /* 
- * Start Date: 
- * End Date: 
+ * Start Date: 12:06 PM, 26 April 2022 
+ * End Date: 1:15 PM, 26 April 2022
  * Author: Al Nahian (alnahian2003)
  * Topic: Working With Exceptions in PHP
  */
+
+function greet($name)
+{
+    if ((is_numeric($name) or is_countable($name) or is_bool($name)) == true) {
+        throw new Exception("Please give your name!");
+    }
+    return "Hello, $name!";
+}
+try {
+    echo greet("Al Nahian");
+} catch (\Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+echo "<br>";
+
+function getAge($birthYear)
+{
+    if ($birthYear > date("Y")) {
+        throw new Exception("Birth Year can't be ahead of the current year");
+    }
+    return date("Y") - $birthYear;
+}
+
+
+try {
+    echo getAge(2003);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
