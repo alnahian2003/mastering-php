fdd8864d0e327c69d798dc64fa7b72d3e10932ff
Al Nahian <a.alnahian2003@gmail.com>
2022-04-30 01:41:26 +0600

    Working with Constants in OOP PHP completed

--- advanced/oop/constants.php
+++ advanced/oop/constants.php
@@ -1,8 +1,24 @@
-<?php 
+<?php
 /* 
 * Start Date: 1:30 AM, 30 April 2022
-* End Date: 
+* End Date: 1:41 AM, 30 April 2022
 * Author: Al Nahian (alnahian2003)
 * Topic: Working with Constants in OOP PHP
 */
 
+class greet
+{
+
+    const USER = "Al Nahian";
+
+    public function goodMorning()
+    {
+        echo "Good Morning, " . self::USER;
+    }
+}
+
+$greeting = new greet();
+
+$greeting->goodMorning();
+echo nl2br("\n");
+echo "This is from outside of the class: Hello " . greet::USER;
